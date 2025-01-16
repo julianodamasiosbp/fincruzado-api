1c6ce40445ddeb1d001c4b1e45dbe7515494ebcf
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-01-16 13:20:19 +0000

    chore: add `spring project`

---
+++ fincruzado/src/main/java/br/com/fincruzado/fincruzado/FincruzadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fincruzado.fincruzado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincruzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincruzadoApplication.class, args);
+	}
+
+}
---
+++ fincruzado/src/test/java/br/com/fincruzado/fincruzado/FincruzadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fincruzado.fincruzado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincruzadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
